d1fa7d4b0399f65d48c5c88243708b44d72e9665
QueenterJuma <jumahqueenter@gmail.com>
2023-10-07 21:03:14 +0300

    Add Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react';
-import  styles from './Navbar.module.css';
-
+import styles from './Navbar.module.css';
 
 function Navbar() {
-
-  // adding the states 
+  // adding the states
   const [isActive, setIsActive] = useState(false);
 
-  //add the active class
+  // add the active class
   const toggleActiveClass = () => {
     setIsActive(!isActive);
   };
 
-  //clean up function to remove the active class
-  
+  // clean up function to remove the active class
+
   const removeActive = () => {
-    setIsActive(false)
+    setIsActive(false);
   };
 
   return (
@@ -25,27 +23,27 @@ function Navbar() {
         <nav className={`${styles.navbar}`}>
 
           {/* logo */}
-          <a href='#home' className={`${styles.logo}`}>Dev. </a>
+          <a href="#home" className={`${styles.logo}`}>Dev. </a>
 
           <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
             <li onClick={removeActive}>
-              <a href='#home' className={`${styles.navLink}`}>Home</a>
+              <a href="#home" className={`${styles.navLink}`}>Home</a>
             </li>
             <li onClick={removeActive}>
-              <a href='#home' className={`${styles.navLink}`}>Catalog</a>
+              <a href="#home" className={`${styles.navLink}`}>Catalog</a>
             </li>
             <li onClick={removeActive}>
-              <a href='#home' className={`${styles.navLink}`}>All products</a>
+              <a href="#home" className={`${styles.navLink}`}>All products</a>
             </li>
             <li onClick={removeActive}>
-              <a href='#home' className={`${styles.navLink}`}>Contact</a>
+              <a href="#home" className={`${styles.navLink}`}>Contact</a>
             </li>
           </ul>
 
-          <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`}  onClick={toggleActiveClass}>
-            <span className={`${styles.bar}`}></span>
-            <span className={`${styles.bar}`}></span>
-            <span className={`${styles.bar}`}></span>
+          <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`} onClick={toggleActiveClass}>
+            <span className={`${styles.bar}`} />
+            <span className={`${styles.bar}`} />
+            <span className={`${styles.bar}`} />
           </div>
         </nav>
 
