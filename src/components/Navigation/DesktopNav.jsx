3ae396acf0c57fd79e7a1d24a0800577e2ab37e8
stephenkati <stephenkatuli88@gmail.com>
2023-11-02 03:34:21 +0300

    Add react popup menu

--- src/components/Navigation/DesktopNav.jsx
+++ src/components/Navigation/DesktopNav.jsx
@@ -1,29 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const DesktopNav = () => {
   const links = [
-    {path:'/', text:'HOME'},
-    {path:'/news', text:'NEWS'},
-    {path:'/about', text:'ABOUT US'},
-    {path:'/contact', text:'CONTACT'}
+    { text: 'HOME', path:'#', options: [{optionName: 'Mission', path: '/mission'}, {optionName: 'Vision', path: '/vision'}] },
+    { text: 'NEWS', path:'/news', options: [{optionName: 'Events', path: '/events'}, {optionName: 'Notices', path: '/notices'}] },
+    { text: 'RESOURCES', path:'/resources', options: [{optionName: 'Terms & Policy', path: '/terms'}, {optionName: 'Nomination', path: '/nomination'}] },
+    { text: 'ABOUT', path:'/about', options: [{optionName: 'History', path: '/history'}, {optionName: 'Value', path: '/value'}, { optionName: 'Our Team', path: '/team'}] },
+    { text: 'CONTACT', path:'/contact', options: [] }
   ];
+
+  const [selectedLink, setSelectedLink] = useState(null);
+
+  const handleLinkClick = (link) => {
+    setSelectedLink(link);
+  };
 
   return (
     <ul className='menu-list-desktop'>
-      {links.map(link => (
-        <li key={link.path}>
+      {links.map((link, index) => (
+        link.options.length > 0 ? (
+          <Popup
+            key={index}
+            trigger={
+              <Link
+                to={link.path}
+                className='menu-link-desktop'
+                onClick={() => handleLinkClick(link)}
+              >
+                {link.text}
+              </Link>
+            }
+            position="bottom start"
+            contentStyle={{ width: 'max-content' }}
+          >
+            <div className='drop-down'>
+              {
+                link.options.map((subLink )=> (
+                  <Link 
+                    to={subLink.path}
+                    key={subLink.optionName}
+                    className='drop-item'
+                  >
+                    {subLink.optionName}
+                  </Link>
+                ))
+              }
+            </div>
+          </Popup>
+        ) : (
           <Link
+            key={index}
             to={link.path}
             className='menu-link-desktop'
+            onClick={() => handleLinkClick(link)}
           >
-              {link.text}
+            {link.text}
           </Link>
-        </li>
+        )
       ))}
+
     </ul>
-  )
-}
+  );
+};
 
 export default DesktopNav;
