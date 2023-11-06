c8fca420393d7bfd5c1dda8debbae42d00f961d9
stephenkati <stephenkatuli88@gmail.com>
2023-11-06 14:56:42 +0300

    Add register section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Homepage/Hero';
+import Register from '../components/Homepage/Register';
 
 const Home = () => {
   return (
     <div className='homepage'>
       <Hero />
+      <Register />
     </div>
   )
 }
