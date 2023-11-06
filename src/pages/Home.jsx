165ab8e9a5fc572b59871e82307453cfad942bd3
stephenkati <stephenkatuli88@gmail.com>
2023-11-06 11:23:08 +0300

    Create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/Homepage/Hero';
+
+const Home = () => {
+  return (
+    <div className='homepage'>
+      <Hero />
+    </div>
+  )
+}
+
+export default Home;
