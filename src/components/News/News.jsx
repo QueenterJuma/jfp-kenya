5d8ea8f1dce667caf2e8c540a88688df0bd28ba4
stephenkati <stephenkatuli88@gmail.com>
2023-11-08 12:35:26 +0300

    Create News component

---
+++ src/components/News/News.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {NewsList} from './NewsList';
+
+const News = () => {
+  const firstNews = NewsList.slice(0, 1);
+  const secondNews = NewsList.slice(1, 3);
+
+  return (
+    <div className='news'>
+      <h3> LATEST NEWS </h3>
+
+      <div className='news-cont'>
+        {firstNews.map((news, index) => (
+          <div key={index} className='pri-news'>
+            <div className='pri-news-cont'>
+              <h4> {news.title} </h4>
+              <p> {news.description} </p>
+              <a href=''>READ MORE</a>
+            </div>
+            <div className='pri-news-img-cont'>
+              <img src={news.image} alt={news.title} />
+            </div>
+          </div>
+        ))}
+
+        <div className='other-news'>
+          {secondNews.map((news, index) => (
+            <div key={index} className='card-layout'>
+              <div className='other-news-img-cont'>
+                <img src={news.image} alt={news.title} />
+              </div>
+              <div className='other-news-cont'>
+                <h4> {news.title} </h4>
+                <p> {news.description.slice(0, 100)}... </p>
+                <a href=''>READ MORE</a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default News;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Hero from '../components/Homepage/Hero';
 import Register from '../components/Homepage/Register';
 import Team from '../components/About/Team';
+import News from '../components/News/News';
 
 const Home = () => {
   return (
     <div className='homepage'>
       <Hero />
+      <News />
       <Register />
       <Team />
     </div>
