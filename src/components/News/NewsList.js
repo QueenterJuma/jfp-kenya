1e43ab5e681148863047d69098d5762bb5d78074
stephenkati <stephenkatuli88@gmail.com>
2023-11-08 12:35:37 +0300

    Create a list of news

---
+++ src/components/News/NewsList.js
@@ -0,0 +1,17 @@
+export const NewsList = [
+  {
+    title: 'Political Event X',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
+    image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
+  },
+  {
+    title: 'New Government Policy Announcement',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. The government announced a new policy aimed at addressing key issues in the country, garnering mixed reactions from the public.',
+    image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
+  },
+  {
+    title: 'Election Results',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. The results of the recent elections are in, and they will have a major influence on the future of our nation.',
+    image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
+  }
+];
