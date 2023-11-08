6ec426af27e8b8f6207528e8692283c4adcf2b34
stephenkati <stephenkatuli88@gmail.com>
2023-11-08 16:51:55 +0300

    Add mission and vission

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,15 @@ import Hero from '../components/Homepage/Hero';
 import Register from '../components/Homepage/Register';
 import Team from '../components/About/Team';
 import News from '../components/News/News';
+import Mission from '../components/Mission/Mission';
 
 const Home = () => {
   return (
     <div className='homepage'>
       <Hero />
-      <News />
+      <Mission />
       <Register />
+      <News />
       <Team />
     </div>
   )
