c63cf4e092afc0669dcdf0b2a8d2d2e97f293615
stephenkati <stephenkatuli88@gmail.com>
2023-11-13 13:48:30 +0300

    Setup leaflet framework

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 
 const Map = () => {
-  const position = [51.505, -0.09]
+  const position = [51.505, -0.09];
 
   return (
-    <MapContainer center={position} zoom={13}>
+    <MapContainer  
+      center={{ lat: 51.505, lng: -0.09 }}
+      zoom={15}
+      className='map'
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      >
-        <Marker position={position}>
-          <Popup>
-            Hello
-          </Popup>
-        </Marker>
-      </TileLayer>
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
-  )
-}
+  );
+};
 
 export default Map;
