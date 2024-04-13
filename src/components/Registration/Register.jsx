cf1c7ae70e532bee98a3fd6ba8b11f9a68f1d9af
QueenterJuma <jumahqueenter@gmail.com>
2024-04-13 18:32:14 +0300

    Move register.jsx to Registration component

---
+++ src/components/Registration/Register.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { TbStarsFilled } from 'react-icons/tb'
+const Register = () => {
+  return (
+    <div className='register'>
+      <section>
+        <div>
+          <span className='stars'> <TbStarsFilled /> </span>
+          <h2>Join the JFP Membership Today!</h2>
+          <p>Experience the party that genuinely cares about you.</p>
+        </div>
+        <div>
+          <a href=''>REGISTER</a>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Register;
