8eb4d15f51b9376103e3069e09651ba04a57557b
QueenterJuma <jumahqueenter@gmail.com>
2025-01-04 16:28:36 +0300

    Add update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <Route path='/resources' element={ <Resources /> } />
         {/*  <Route path='/about' element={ <About /> } />
           <Route path='/contact' element={ <Contact /> } /> */}
-        </Routes>
+        </Routes>        
 
         <Navbar />
         <Footer />
