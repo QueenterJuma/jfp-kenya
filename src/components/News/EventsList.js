04141243d82dc3b9d55e8234dc09bc95ca77ceb2
Tracy-miranja <barazatracy44@gmail.com>
2025-01-25 23:04:20 +0300

    Modify news section

--- src/components/News/EventsList.js
+++ src/components/News/EventsList.js
@@ -3,15 +3,15 @@ export const EventsList = [
     title: 'Political Event X',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
     image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
+  },
+  {
+    title: 'Political Event X',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
+    image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
+  },
+  {
+    title: 'Political Event X',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
+    image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
   }
-  // {
-  //   title: 'Political Event X',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
-  //   image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
-  // },
-  // {
-  //   title: 'Political Event X',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
-  //   image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
-  // }
 ];
