1887ade84a8e1021a5429e0e281de9561f53a744
QueenterJuma <jumahqueenter@gmail.com>
2025-01-25 17:26:58 +0300

    Add Style.css update

--- src/components/News/EventsList.js
+++ src/components/News/EventsList.js
@@ -4,4 +4,14 @@ export const EventsList = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
     image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
   }
+  // {
+  //   title: 'Political Event X',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
+  //   image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
+  // },
+  // {
+  //   title: 'Political Event X',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In political news today, Event X took place, and it had a significant impact on the political landscape.',
+  //   image: 'https://www.everythingbranded.com/cdn-cgi/image/quality=70/slir/w1120-h512-c6x3/assets/media/2023/08/10/political-merchandise-ideas-for-your-election-campaign.png',
+  // }
 ];
--- src/components/News/NewsList.js
+++ src/components/News/NewsList.js
@@ -2,9 +2,18 @@ import Statement2023 from '../../assets/statement2023-24.png'
 
 export const NewsList = [
   {
-    title: 'Congratulations to JFP on the Approval of Our Financial Statement for FY 2023/24',
-    description: 'We are delighted to announce that the Office of the Registrar of Political Parties (ORPP) has officially acknowledged and approved the Justice and Freedom Party (JFP) financial statements for the financial year 2023/24. This milestone reflects our unwavering commitment to transparency, accountability, and adherence to legal requirements as outlined in Section 29 of the Political Parties Act, 2011.',
+    title: 'Approval of Financial Statement for FY 2023/24',
+    description: 'Congratulations to JFP for the approval of our FY 2023/24 financial statements by the Office of the Registrar of Political Parties (ORPP), reaffirming our dedication to transparency, accountability, and compliance with Section 29 of the Political Parties Act, 2011.',
     image: Statement2023,
   }
-  
+  // {
+  //   title: 'Congratulations to JFP on the Approval of Our Financial Statement for FY 2023/24',
+  //   description: 'We are delighted to announce that the Office of the Registrar of Political Parties (ORPP) has officially acknowledged and approved the Justice and Freedom Party (JFP) financial statements for the financial year 2023/24. This milestone reflects our unwavering commitment to transparency, accountability, and adherence to legal requirements as outlined in Section 29 of the Political Parties Act, 2011.',
+  //   image: Statement2023,
+  // },
+  // {
+  //   title: 'Congratulations to JFP on the Approval of Our Financial Statement for FY 2023/24',
+  //   description: 'We are delighted to announce that the Office of the Registrar of Political Parties (ORPP) has officially acknowledged and approved the Justice and Freedom Party (JFP) financial statements for the financial year 2023/24. This milestone reflects our unwavering commitment to transparency, accountability, and adherence to legal requirements as outlined in Section 29 of the Political Parties Act, 2011.',
+  //   image: Statement2023,
+  // }
 ];
