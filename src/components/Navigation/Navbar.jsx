7a30fc3039c8973a4fa14fb1a348b38b88eb8ab2
QueenterJuma <jumahqueenter@gmail.com>
2025-01-27 14:10:00 +0300

    Add resource Style update

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MobileNav from './MobileNav';
 import DesktopNav from './DesktopNav';
-import Logo from '/logo.png'
+import Logo from '/log.png'
 
 const Navbar = () => {
   return (
