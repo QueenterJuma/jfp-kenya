a2e395e1c943a192105a278548276148598282b3
QueenterJuma <jumahqueenter@gmail.com>
2025-02-18 08:27:46 +0300

    Add update in Missision and Vision statement

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -9,12 +9,12 @@ const Mission = () => {
         <h2>Mission</h2>
         <span> <GiBullseye /> </span>
         <p>
-          To be a conduit to equip, nature and protect social and democratic society for edifying of the citizen to unity till we be complete and effective globally.
+          To be a conduit to equip, nurture and protect social and democratic society for edifying of the citizen to unity till we be complete and effective globally.
         </p>
       </div>
 
       <div className='vission'>
-        <h2>Vission</h2>
+        <h2>Vision</h2>
         <span> <FaPrayingHands /> </span>
         <p>
           Restoration of justice in the land to establish a prosperous, industrialized and modern nation, in which all citizens enjoy equal social, economic and democratic rights, and live in unity.
