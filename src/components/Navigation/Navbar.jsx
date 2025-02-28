43b44bcb070c67f200f4644aae4e716bd4ede781
QueenterJuma <jumahqueenter@gmail.com>
2025-03-01 00:23:35 +0300

    Add update

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import MobileNav from './MobileNav';
 import DesktopNav from './DesktopNav';
-import Logo from '/log1.jpg';
+// import Logo from '/log1.jpg';
 
 const Navbar = () => {
   return (
     <div className='nav-cont'>
       <div className='nav'>
-        <div className='logo-cont'> <img src={Logo} alt='JFP Logo' className='logo' /></div>
+        {/* <div className='logo-cont'> <img src={Logo} alt='JFP Logo' className='logo' /></div> */}
         <div className='menu' id='mobile-menu'><MobileNav /></div>
         <div className='menu' id='desktop-menu'><DesktopNav /></div>
       </div>
