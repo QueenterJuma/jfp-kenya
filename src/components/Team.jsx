067b3068c7e35d7ca9d501071129556ddb25a6fa
QueenterJuma <jumahqueenter@gmail.com>
2025-03-12 23:54:39 +0300

    Add update on Team list and roles

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,8 +1,10 @@
-import Team1 from '../assets/Team1-Isaac.jpg'
-import Team2 from '../assets/Team2-Jennifer.jpg'
-import Team3 from '../assets/Team3-Carroly.jpg'
-import Team4 from '../assets/Team4-Hellen.jpg'
-import Team5 from '../assets/Team5-Kenneth.jpg'
+import Team1 from '../assets/Team1-Justus.png'
+import Team2 from '../assets/Team2-Ruben.png'
+import Team3 from '../assets/Team3-Jennifer.jpg'
+import Team4 from '../assets/Team4-Isaac.jpg'
+import Team5 from '../assets/Team5-Hellen.jpg'
+import Team6 from '../assets/Team6-Joy.png'
+
 
 export default function Team() {
   return (
@@ -28,12 +30,12 @@ export default function Team() {
   );
 }
 
-// team data (Replace with actual images)
+//team data 
 const teamData = [
-  { name: "Isaac Aluoch Aluochier", role:"Chairman", image: Team1},
-  { name: "Jennifer Njeri Kanari", role:"Secretary General", image: Team2},
-  { name: "Carroly's Omulo", role:"Deputy Sec. General", image: Team3 },
-  { name: "Hellen Akoth Omolo Mtawali", role:"Organizing Secretary", image: Team4 },
-  { name: "Kenneth Mburu Nyoike", role:"Deputy Organizing Secretary", image: Team5 },
-
+  { name: "Justus Juma", role:"Party Leader", image: Team1},
+  { name: "Ruben Kigame", role:"Deputy-Party Leader", image: Team2},
+  { name: "Isaac Aluoch Aluochier", role:"Chairman", image: Team3},
+  { name: "Jennifer Njeri Kanari", role:"Secretary General", image: Team4},
+  { name: "Hellen Akoth Omolo Mtawali", role:"Organizing Secretary", image: Team5},
+  { name: "Joy Kiruki", role:"Treasure", image: Team6 },
 ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Homepage/Hero';
 import Register from '../components/Registration/Register';
-import Team from '../components/About/Team';
+import Team from '../components/About/Teaml';
 import News from '../components/News/News';
 import Mission from '../components/Mission/Mission';
 
