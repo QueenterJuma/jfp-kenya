86cfa4d74ef134e7866270eec45991294e48b481
QueenterJuma <jumahqueenter@gmail.com>
2025-07-17 13:06:06 +0300

    Add donation page

---
+++ src/pages/Donate.jsx
@@ -0,0 +1,31 @@
+export default function Donate() {
+    return (
+        <div className="donate-container">
+            <h1>Support the Justice and Freedom Party</h1>
+            <p>
+            Your donation will help us strengthen democracy, empower communities, and ensure justice for all.
+            </p>
+
+            <div className="donation-details">
+                <h2>Bank Transfer</h2>
+                <ul>
+                    <li><strong>Bank Name:</strong> Kenya Commercial Bank (KCB)</li>
+                    <li><strong>Recipient:</strong> Justice And Freedom Party of Kenya</li>
+                    <li><strong>Account Number:</strong> 1205216049</li>
+                    <li><strong>Branch:</strong> Lavington</li>
+                    <li><strong>Bank Code:</strong> 01</li>
+                    <li><strong>Branch Code:</strong> 01320</li>
+                    <li><strong>SWIFT Code:</strong> KCBLKENX</li>
+                </ul>
+
+                <h2>MPESA PayBill</h2>
+                <ul>
+                    <li><strong>PayBill Number:</strong> 522522</li>
+                    <li><strong>Account Number:</strong> 1205216049</li>
+                </ul>
+
+            </div>
+        </div>
+        
+    )
+}
