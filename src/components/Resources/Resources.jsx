bdb141d2da32f088dafe832c37259edf7ef0fc3a
QueenterJuma <jumahqueenter@gmail.com>
2025-10-31 14:47:44 +0300

    Add  Statement document

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BsDownload } from 'react-icons/bs';
+import NDCNotice from '/NDC-Notice.pdf';
+import NECOFFICIALS from '/NEC-Officials.pdf';
 import CONSTITUTION from '/Constitution.pdf';
+import TERMS from '/Terms-Conditions.pdf';
 import REGISTRATION from '/Reg-Procedure.pdf';
 import BALANCE from '/Financials-tri-balance.pdf';
-import IPSAS from '/Annual-IPSAS-Accrual.pdf'
-import STATEMENT2023 from '/Stateme2023-24.png'; 
+import IPSAS from '/Annual-IPSAS-Accrual.pdf';
+import STATEMENT2023 from '/Stateme2023-24.pdf'; 
 
 const Resources = () => {
   return (
@@ -12,13 +15,39 @@ const Resources = () => {
       <h1>Resources</h1>
 
       <p>Welcome to the Justice and Freedom Party (JFP) Resources Page, your go-to hub for essential documents, policies, and educational materials. Here, you will find valuable resources that outline our vision, mission, and strategies for a just and free society.</p>
+     
+      <a href={NDCNotice} target='_blank' download> <button
+        type='button'
+        className=""
+        title="NDCnotice"
+      >
+        NDC NOTICE <BsDownload />
+      </button> </a>
+
+      <a href={NECOFFICIALS} target='_blank' download> <button
+        type='button'
+        className=""
+        title="Nec"
+      >
+        NEC OFFICIALS <BsDownload />
+      </button> </a>
+      
      <a href={CONSTITUTION} target='_blank' download> <button
         type='button'
         className=""
         title="Constitution"
       >
-        Constitution <BsDownload />
+        CONSTITUTION <BsDownload />
       </button> </a>
+
+      <a href={TERMS} target='_blank' download> <button
+        type='button'
+        className=""
+        title="CTerms"
+      >
+        TERMS & CONDITION <BsDownload />
+      </button> </a>
+
 
       <a href={REGISTRATION} target='_blank' download><button
         type='button'
