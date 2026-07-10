81bc01581878cf3eecce33c8530cd40ddd2aef6b
QueenterJuma <jumahqueenter@gmail.com>
2026-07-11 01:37:03 +0300

    Add update team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,10 +1,11 @@
 import Team1 from '../assets/Team1-Justus.png'
-import Team6 from '../assets/Team6-Gidhii.png'
+import Team6 from '../assets/Team6-Idah.png'
 import Team2 from '../assets/Team2-Ogendo.png'
 import Team3 from '../assets/Team3- Mohamed.png'
 import Team4 from '../assets/Hellen-Taam4.png'
 import Team5 from '../assets/Team5-Joy.png'
-
+import Team7 from '../assets/Team7-Pauline.jpg'
+// import Team8 from '../assets/Team8-Queenter.png'
 
 export default function Team() {
   return (
@@ -35,9 +36,11 @@ export default function Team() {
 // Team data 
 const teamData = [
   { name: "Justus Juma", role: "Party Leader", image: Team1 },
-  {name: "David Gidhii", role: "Deputy Party Leader", image: Team6},
+  {name: "Idah Muathime Muema", role: "Deputy Party Leader", image: Team6},
   { name: "Cervin Ogendo", role: "Secretary General", image: Team2 },
   { name: "Zeinab Hussein Mohamed", role: "National Chairman", image: Team3 },
   { name: "Hellen Mutawali", role: "Organizing Secretary", image: Team4 },
   { name: "Joy Kiruki", role: "Treasurer", image: Team5 },
+  { name: "Pauline Uside", role: "Women Leader", image: Team7 },
+  // { name: "Queenter Anyango", role: "Youth Leader", image: Team8 },
 ];
